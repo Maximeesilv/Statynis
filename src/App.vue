437f36d93c0eb9c63ac672698c750504eb99eb85
maximeesilv <maxime.gasnier.1@gmail.com>
2021-01-08 05:40:04 -0400

    Init Project + Add PlayerDetails page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div id="app" class="container">
+    <b-navbar>
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                    alt="Lightweight UI components for Vue.js based on Bulma"
+                >
+            </b-navbar-item>
+      </template>
+    </b-navbar>
+    <router-view class="--full-page-height"/>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          Work done by <strong>Maxime Gasnier</strong> Dev @Oxynum.
+        </p>
+      </div>
+  </footer>
   </div>
 </template>
 
 <style lang="scss">
+@import 'styles/global.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/atoms/BasicButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <b-button rounded @click="emitClick">
+        {{ text }}
+    </b-button>
+</template>
+
+<script>
+export default {
+    name: 'BasicButton',
+    props: {
+        text: {
+            type: String,
+            default: () => ''
+        }
+    },
+    methods: {
+        emitClick() {
+            this.$emit('hasBeenClicked')
+        }
+    }
+}
+</script>
